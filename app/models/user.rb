b69c10d188a10f8123af0945bc16ab8c876cd914
rajneesh-vinsol <rajneesh@vinsol.com>
2018-10-31 12:09:47 +0530

    add scope to i18n

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,17 @@
 class User < ApplicationRecord
   
-  ROLES = { customer: 0, admin: 1 }
-  enum role: ROLES
+  ROLES = ({ customer: 0, admin: 1 }).freeze
+
   has_secure_password
 
+  enum role: ROLES
+
+  # Validations
   validates :name, presence: true
   validates :email, presence: true
-  validates :email, :uniqueness => { :case_sensitive => false }, format: {
+  validates :email, uniqueness: { case_sensitive: false }, format: {
     with:    EMAIL_REGEXP,
-    message: I18n.t('.invalid_email')
+    message: :invalid_email
   }, allow_blank: true
 
 end
