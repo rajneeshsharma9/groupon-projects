29ac99596aac28030dd3663834ac9e4d10fba697
rajneesh-vinsol <rajneesh@vinsol.com>
2018-11-14 11:35:03 +0530

    create separate admin js/css files

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,13 +1,15 @@
-class Admin::BaseController < ApplicationController
-
-  before_action :ensure_user_is_admin
+module Admin
+  class BaseController < ApplicationController
   
-  layout 'admin/layouts/admin'
-  #to check if user is admin
-  private def ensure_user_is_admin
-    unless current_user.admin?
-      redirect_to home_page_path, danger: t('.not_admin')
+    before_action :ensure_user_is_admin
+    
+    layout 'admin'
+    #to check if user is admin
+    private def ensure_user_is_admin
+      unless current_user.admin?
+        redirect_to home_page_path, danger: t('.not_admin')
+      end
     end
+  
   end
-
 end
