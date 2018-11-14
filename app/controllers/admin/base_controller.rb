13a7b253cb41a02da60407ac673f4600f92072c1
rajneesh-vinsol <rajneesh@vinsol.com>
2018-11-14 10:32:41 +0530

    add a comment to see codeclimate issues

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,7 +3,7 @@ class Admin::BaseController < ApplicationController
   before_action :ensure_user_is_admin
   
   layout 'admin/layouts/admin'
-
+  #to check if user is admin
   private def ensure_user_is_admin
     unless current_user.admin?
       redirect_to home_page_path, danger: t('.not_admin')
