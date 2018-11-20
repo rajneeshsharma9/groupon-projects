ed561d8ae98f06d4555f5edc0b3b4e1706a4d4a8
rajneesh-vinsol <rajneesh@vinsol.com>
2018-11-21 02:58:55 +0530

    add partial for images

--- app/assets/javascripts/admin/multiple_input_field_handler.js
+++ app/assets/javascripts/admin/multiple_input_field_handler.js
@@ -6,16 +6,17 @@ function MultipleInputFieldHandler(options) {
 
 MultipleInputFieldHandler.prototype.addInputFieldToContainer = function() {
   var inputDiv = this.hiddenInputDiv.clone().removeClass("hidden-input-div"),
-      _this = this;
+      _this = this,
+      currentTimeStamp = $.now();
   inputDiv.find("[data-type='add-more-field']").each(function(index, input) {
     $input = $(input);
-    $input.attr('name', _this.returnNameFieldValue($input));
+    $input.attr('name', _this.returnNameFieldValue($input, _this.currentTimeStamp));
   });
   this.container.append(inputDiv);
 }
 
-MultipleInputFieldHandler.prototype.returnNameFieldValue = function(input) {
-  return input.data("model") + '[' + input.data("association") + '][' + $.now() + ']';
+MultipleInputFieldHandler.prototype.returnNameFieldValue = function(input, currentTimeStamp) {
+  return input.data("model") + '[' + input.data("association") + '][' + currentTimeStamp + ']';
 }
 
 MultipleInputFieldHandler.prototype.addButtonClickHandler = function(event) {
@@ -32,10 +33,11 @@ MultipleInputFieldHandler.prototype.init = function() {
 };
 
 $(document).ready(function() {
+  var addMoreContainer = $("[data-type='add-more-images-container'");
   var options = {
-    "button" : $("[data-type='add-more-images-container'").find("[data-type='add-more-button']"),
-    "hiddenInputDiv" : $("[data-type='add-more-images-container'").find("[data-type='hidden-input-div']"),
-    "container" : $("[data-type='add-more-images-container'").find("[data-type='add-more-container']")
+    "button" : addMoreContainer.find("[data-type='add-more-button']"),
+    "hiddenInputDiv" : addMoreContainer.find("[data-type='hidden-input-div']"),
+    "container" : addMoreContainer.find("[data-type='add-more-container']")
   },
 
     handler = new MultipleInputFieldHandler(options);
