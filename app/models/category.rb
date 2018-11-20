f1b635d965bae1215d87550c2b05f636a879b559
rajneesh-vinsol <rajneesh@vinsol.com>
2018-11-20 18:59:34 +0530

    add category and location to deal

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,6 @@ class Category < ApplicationRecord
   # Associations
   has_many :deals
   # Validations
-  validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }, allow_blank: true
 
 end
