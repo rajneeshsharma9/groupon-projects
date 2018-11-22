62961feba4d8d24fd568032ecaeb5d201bbb2d43
rajneesh-vinsol <rajneesh@vinsol.com>
2018-11-22 19:07:15 +0530

    fix rebase issues

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -15,8 +15,8 @@ class Deal < ApplicationRecord
   # Validations
   validates :title, presence: true
   validates :title, uniqueness: { case_sensitive: false }, allow_nil: true
-  validates :minimum_purchases_required, numericality: { greater_than_or_equal_to: 0 }
-  validates :maximum_purchases_allowed, numericality: { greater_than: :minimum_purchases_required }
+  validates :minimum_purchases_required, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :maximum_purchases_allowed, numericality: { greater_than: :minimum_purchases_required }, allow_nil: true
   validates :start_at, :expire_at, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: MINIMUM_ALLOWED_PRICE }, allow_nil: true
   validates :price, numericality: { less_than_or_equal_to: MAXIMUM_ALLOWED_PRICE }, allow_nil: true
