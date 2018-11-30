feee7a953347ed7907b3644bb81cfb3c9bf89353
rajneesh-vinsol <rajneesh@vinsol.com>
2018-11-30 11:51:06 +0530

    add order table

---
+++ db/migrate/20181130053123_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.references :user
+      t.decimal :amount, precision: 7, scale: 2, default: 0
+      t.string :workflow_state
+      t.string :receiver_email
+      t.timestamps
+    end
+  end
+end
