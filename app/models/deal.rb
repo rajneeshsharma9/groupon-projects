451f76574cdc292de13db7c5183708ed25459941
rajneesh-vinsol <rajneesh@vinsol.com>
2018-12-01 17:21:14 +0530

    add condition to search scope

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -37,7 +37,7 @@ class Deal < ApplicationRecord
   scope :available_for_collection, ->(collection) { where(collection_id: nil, published_at: nil).or(Deal.where(collection_id: collection.id)) }
   scope :published, -> { where.not(published_at: nil) }
   scope :filter, ->(filters) { where(filters) }
-  scope :search, ->(search) { joins(locations: :address).where(Address.arel_table[:city].matches("#{search}%").or(Deal.arel_table[:title].matches("#{search}%")).to_sql) }
+  scope :search, ->(search) { joins(locations: :address).where(Address.arel_table[:city].matches("#{search}%").or(Deal.arel_table[:title].matches("#{search}%")).to_sql) if search.present? }
 
   def publish
     update(published_at: Time.current)
