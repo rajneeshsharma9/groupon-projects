0d9c5d2ce6c7fb9797a728fb78b9c0198ac1680d
rajneesh-vinsol <rajneesh@vinsol.com>
2018-12-05 00:37:18 +0530

    refactor set order

--- app/controllers/concerns/current_cart_order.rb
+++ app/controllers/concerns/current_cart_order.rb
@@ -2,14 +2,22 @@ module CurrentCartOrder
 
   private def set_cart_order
     if logged_in?
-      @order = current_user.current_order
-      @order ||= current_user.orders.create
+      set_user_order
     else
-      @order = Order.find_by(id: session[:order_id])
-      unless @order
-        @order = Order.create
-        session[:order_id] = @order.id
-      end
+      set_session_order
+    end
+  end
+
+  private def set_user_order
+    @order = current_user.current_order
+    @order ||= current_user.orders.create
+  end
+
+  private def set_session_order
+    @order = Order.find_by(id: session[:order_id])
+    unless @order
+      @order = Order.create
+      session[:order_id] = @order.id
     end
   end
 
